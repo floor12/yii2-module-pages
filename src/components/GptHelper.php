8b0ccf1cd4a3091c0909fcb84385e47e375c179d
floor12 <floor12@floor12.net>
2025-04-23 11:27:24 +0200

    GPT model updated to more cheap.

--- src/components/GptHelper.php
+++ src/components/GptHelper.php
@@ -15,7 +15,7 @@ class GptHelper
         $client = self::getClient();
         $request = 'You are SEO especialist. Make data for H1 (h1), Page title (title) and short page meta description (description, 160 letters max) using page language. Give answer in clean json format. Page content:' . strip_tags($content);
         $result = $client->chat()->create([
-            'model' => 'gpt-4o',
+            'model' => 'gpt-4o-mini',
             'messages' => [
                 ['role' => 'user', 'content' => $request],
             ],
@@ -30,7 +30,7 @@ class GptHelper
         $request = 'You are SEO especialist and copywriter. ' . $query . '. Give answer in clean and formatted html. Use only formatting tags (h1, h2, h3, p, ul, li, strong, em etc) and start from h1, dont make html, body, head, title tags. Use current language:' . $lang;
 
         $result = $client->chat()->create([
-            'model' => 'gpt-4',
+            'model' => 'gpt-4o-mini',
             'messages' => [
                 ['role' => 'user', 'content' => $request],
             ],
